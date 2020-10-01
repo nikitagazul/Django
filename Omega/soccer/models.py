305aad5b25ec6e90efdd29c53b845a391c268706
Mykyta Gazul <nikitagazul@gmail.com>
2020-10-01 12:12:26 -0400

    updated table and records

--- Omega/soccer/models.py
+++ Omega/soccer/models.py
@@ -5,15 +5,15 @@ class News(models.Model):
     #News
     image = models.ImageField("Image", upload_to="news/")
     title = models.CharField("Title", max_length=30)
-    date = models.CharField("Date", max_length=12)
+    date = models.DateField("Date")
     description = models.TextField("Description")
-    url = models.SlugField(max_length=160, unique=True)
 
 class Player(models.Model):
     #Players
     image =models.ImageField("Image", upload_to="player/")
     fname = models.CharField("First Name", max_length=15)
     lname = models.CharField(verbose_name="Last Name", max_length=15)
+    birth = models.DateField("Date of Birth")
     position = models.CharField("Position", max_length=12)
     number = models.IntegerField("Number")
     description = models.TextField("Description")
@@ -27,7 +27,8 @@ class Match(models.Model):
     image1 = models.ImageField("Image", upload_to="match/")
     team_name1 = models.CharField(verbose_name="First Team Name", max_length=30)
     team_country1 = models.CharField(verbose_name="First Team Country", max_length=20)
-    result = models.CharField("Result", max_length=5)
+    score1 = models.IntegerField(verbose_name="Score Team One")
+    score2 = models.IntegerField(verbose_name="Score Team Two")
     image2 = models.ImageField("Image", upload_to="match/")
     team_name2 = models.CharField(verbose_name="Second Team Name", max_length=30)
     team_country2 = models.CharField(verbose_name="Second Team Country", max_length=20)
@@ -35,5 +36,7 @@ class Match(models.Model):
     def __str__(self):
         return self.team_name1;
         return self.team_country1;
+        return self.score1;
         return self.team_name2;
         return self.team_country2;
+        return self.score2;
