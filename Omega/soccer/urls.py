991f11dd6c0784b5c7807e3c0ae2d5079f6d27fe
Mykyta Gazul <nikitagazul@gmail.com>
2020-10-02 23:49:40 -0400

    fixed list view

--- Omega/soccer/urls.py
+++ Omega/soccer/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("website/players_list.html", views.PlayersView.as_view()),
+    path("", views.PlayersView.as_view()),
     #path("<int:pk>/", views.PlayerDetailView.as_view()),
 ]
