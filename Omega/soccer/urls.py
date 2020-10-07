d98a12b62dbfe55daecc3a2fdba11be8b280bc97
Mykyta Gazul <nikitagazul@gmail.com>
2020-10-07 19:51:23 -0400

    fixed matchas pages, added all records, fixed navbar hamburger menu

--- Omega/soccer/urls.py
+++ Omega/soccer/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home", views.IndexView.as_view(), name='home'),
+    path("", views.IndexView.as_view(), name='home'),
     path("about", views.AboutView.as_view(), name='about'),
     path("contact", views.ContactView.as_view(), name='contact'),
     path("news", views.NewsView.as_view(), name='news'),
