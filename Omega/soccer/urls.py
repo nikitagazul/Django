09786121721bdf8d035d60b2e33ee2e4a3ecf02e
Mykyta Gazul <nikitagazul@gmail.com>
2020-10-07 12:23:12 -0400

    fixed footer, header and rows

--- Omega/soccer/urls.py
+++ Omega/soccer/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name='home'),
-    path("about.html", views.AboutView.as_view(), name='about'),
-    path("contact.html", views.ContactView.as_view(), name='contact'),
-    path("news_list.html", views.NewsView.as_view(), name='news'),
-    path("matches_list.html", views.MatchesView.as_view(), name='matches'),
-    path("players_list.html", views.PlayersView.as_view(), name='players'),
+    path("home", views.IndexView.as_view(), name='home'),
+    path("about", views.AboutView.as_view(), name='about'),
+    path("contact", views.ContactView.as_view(), name='contact'),
+    path("news", views.NewsView.as_view(), name='news'),
+    path("matches", views.MatchesView.as_view(), name='matches'),
+    path("team", views.PlayersView.as_view(), name='players'),
     # path("<int:pk>/", views.PlayerDetailView.as_view()),
 
 ]
